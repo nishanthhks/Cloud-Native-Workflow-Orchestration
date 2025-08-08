7509c8f2b69a0b6e04dbccee19cb825e4b47aa40
nishanthhks <nishanth.kkps@gmail.com>
2025-08-08 12:28:23 +0530

    file change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
   return (
     <main className="container">
       <h1>Cloud-Native-Workflow-Orchestration Deployment</h1>
-      <p><strong>Message Display:</strong> {message}</p>
+      <p><strong>Message:</strong> {message}</p>
       <button onClick={fetchMessage}>Fetch from Server</button>
     </main>
   );
